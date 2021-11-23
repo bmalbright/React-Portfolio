be504711a1bb8aacfe1a3006075e2b90b3d3fb6e
bmalbright <brianalbright@utexas.edu>
2021-11-22 23:12:59 -0600

    made some basic changes to home and portfolio page to check connectivity.

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -18,12 +18,12 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>
+Now is the time for all good men to come to the aid of their country. 
+      </p>
+    </div>
+  );
+}
