e1a07808c89ed1814db5cf6c4ec368d899a23c46
bmalbright <brianalbright@utexas.edu>
2021-11-24 22:32:31 -0600

    changed footer. changed font in header.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,42 @@
 import React from "react";
 import './css/App.css';
+import logo from "./images/icon-76.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Footer () {
   return (
-      <div class="footer">
-      <ul className="footerLinks">
-          <li className="footerItem">
-          <a href="https://www.linkedin.com/in/brian-albright-95017533" 
-              target="_blank" 
-              rel="noopener noreferrer">
-              LinkedIn</a>
-          </li>
-          <li className="footerItem">
-          <a href="https://www.twitter.com/BAlbright01" 
-              target="_blank" 
-              rel="noopener noreferrer">
-              Twitter</a>
-          </li>
-          <li className="footerItem">
-          <a href="https://github.com/bmalbright" 
-              target="_blank" 
-              rel="noopener noreferrer">
-              Github</a>
-          </li>
-      </ul>
-      </div>
+    <div class="footer">
+        <Row>
+            <Col>
+                <img className="img-responsive" src={logo} alt="logo" />
+            </Col>
+            <Col>
+                <a href="https://www.linkedin.com/in/brian-albright-95017533" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                LinkedIn</a>
+            </Col>    
+            <Col>
+                <a href="https://www.twitter.com/BAlbright01" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                Twitter</a>
+            </Col>
+            <Col>
+                <a href="https://github.com/bmalbright" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                Github</a>
+            </Col>
+
+            <Col>
+                <img className="img-responsive" src={logo} alt="logo" />
+            </Col>
+        </Row>
+      </div>  
+
+
   )  
 }
 
