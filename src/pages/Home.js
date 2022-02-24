19dd103786fc655730df3284c47849101801b646
bmalbright <brianalbright@utexas.edu>
2022-02-24 03:34:15 -0600

    added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,80 +1,72 @@
-import React from 'react';
-import {
-
-  Col,
-  Row,
-  Card,
-
-} from 'react-bootstrap';
-
-
+import React from "react";
+import { Col, Row, Card } from "react-bootstrap";
 import "../css/App.css";
+
+import Biopic from "../images/biopic.jpg";
+import Fiona from "../images/fiona.jpg";
 
 export default function Home() {
   return (
     <div className="home">
+      <Row>
+        <Col className="profile">
+          <Card className="biopic">
+            <img className="img-responsive" src={Biopic} alt="Biopic" />
+          </Card>
+        </Col>
+      </Row>
 
-        
-        {/* <Row>
-      
-          <Col className="profile">
-
-          <img className="img-responsive" src={Logo} alt="logo" />
-
-          </Col>
-        
-        </Row> */}
-       
-        <div>
-        
+      <div>
         <Row>
           <Col>
-          <Card className="paragraph">
-            <p >
-            I am a full stack web developer with a management background 
-            looking to build a more intuitive experience for the user. A recent graduate 
-            earning a certificate in full stack web development from the University of 
-            Texas at Austin where I developed skills using HTML, CSS, Javascript, React, 
-            MySQL, MongoDB, Node, and Express. Not only do I consider how the project is 
-            being realized in the present, but also what future development can be added 
-            to make the experience better. I recently worked on projects to build a workout 
-            app to let users create and schedule their workouts, and to build a social 
-            media app that gives users a task or challenge, which they complete by taking 
-            and uploading a picture of. I am excited to use my knowledge and experience to work 
-            on a team building better experiences on the web, whether the user accesses them 
-            on mobile devices or at their desk.  When not at my computer, I can be found at 
-            the park walking my dog, Fiona.
-            </p>
-            </Card>
+            <Card className="bio">
+
+              <h1 className="intro"> Hi, I'm Brian</h1>
+              <p className="paragraph">
+                I am a full stack web developer with a management background
+                looking to build a more intuitive experience for the user. A
+                recent graduate earning a certificate in full stack web
+                development from the University of Texas at Austin where I
+                developed skills using HTML, CSS, Javascript, React, MySQL,
+                MongoDB, Node, and Express. 
+                
+                </p>
+                <p className="paragraph">My previous background in management gives me a unique perspective on the user experience. 
+                  Not only do I consider how the
+                project is being realized in the present, but also what future
+                development can be added to make the experience better. 
+                
+                </p>
+
+                <p className="paragraph">
+                  My recent projects have included:
+                </p>
+                <ul className="paragraph">
+                  <li>A social media app that gives the user a task or challenge, which they complete by taking and uploading a pic</li>
+                  <li>A workout app that allows users to create and schedule their workouts</li>
+                </ul>
+                
+
+                <p className="paragraph"> I am excited to use my
+                knowledge and experience to work on a team building better
+                experiences on the web - whether the user accesses them on mobile
+                devices or at their desk. 
+                </p>
+                <p className="paragraph">When not at my computer, I can be
+                found at the park walking my dog, Fiona.
+              </p>
+
+              </Card>
           </Col>
         </Row>
-        </div>
-        {/* <Row>
-          <Col>
-            <h3 className="title">Full Stack Web Developer</h3>
-            <Row>
-              <Col>
-                <ul>
-                  <li>Javascript</li>
-                  <li>Express</li>
-                  <li>MongoDB</li>
-                  <li>jQuery</li>
-                  <li>MySQL</li>
-                </ul>
-              </Col>
-              <Col>
-                <ul>
-                  <li>React</li>
-                  <li>HTML</li>
-                  <li>Node</li>
-                  <li>Bootstrap</li>
-                  <li>GraphQL</li>
-                </ul>
-              </Col>
-            </Row>
-          </Col>
-        </Row> */}
-
+        <Row>
+        <Col>
+          <Card className="biopic">
+            <img className="img-responsive" src={Fiona} alt="Fiona" />
+          </Card>
+        </Col>
+      </Row>
+      </div>
     </div>
   );
 }
