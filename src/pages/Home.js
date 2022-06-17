1f78623c682118f080ae4ef331591d438a875398
bmalbright <brianalbright@utexas.edu>
2022-06-16 23:59:53 -0500

    edited the about page. Then added an animation.js page that will become the initial landing page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row, Card } from "react-bootstrap";
 import "../css/App.css";
+// import "../css/Home.css";
 
 import Biopic from "../images/biopic.jpg";
 import Fiona from "../images/fiona.jpg";
@@ -23,7 +24,7 @@ export default function Home() {
               <h1 className="intro"> Hi, I'm Brian</h1>
               <p className="paragraph">
                 I am a full stack web developer with a management background
-                looking to build a more intuitive experience for the user. As
+                looking to build a more intuitive experience for the user. As a
                 recent graduate earning a certificate in Full Stack Web
                 Development from the University of Texas at Austin, I developed
                 skills using HTML, CSS, Javascript, React, MySQL, MongoDB, Node,
@@ -36,7 +37,7 @@ export default function Home() {
                 future development can be added to make the experience better.
               </p>
 
-              <p className="paragraph">My recent projects have included:</p>
+              {/* <p className="paragraph">My recent projects have included:</p>
               <ul className="paragraph">
                 <li>
                   A social media app that gives the user a task or challenge,
@@ -46,29 +47,30 @@ export default function Home() {
                   A workout app that allows users to create and schedule their
                   workouts
                 </li>
-              </ul>
+              </ul> */}
             </Card>
           </Col>
         </Row>
         <Row>
-          <Col>
-            <Card className="bio">
-              <p className="paragraph">
+          {/* <Col> */}
+
+          {/* </Col> */}
+          {/* <Col> */}
+            <Card className="fiona">
+              <img className="img-fluid" src={Fiona} alt="Fiona" />
+            </Card>
+          {/* </Col> */}
+          <Card className="bio">
+              {/* <p className="paragraph">
                 I am excited to use my knowledge and experience to work on a
                 team building better experiences on the web - whether the user
                 accesses them on mobile devices or at their desk.
-              </p>
+              </p> */}
               <p className="paragraph">
                 When not at my computer, I can be found at the park walking my
                 dog, Fiona.
               </p>
             </Card>
-          </Col>
-          <Col>
-            <Card className="fiona">
-              <img className="img-fluid" src={Fiona} alt="Fiona" />
-            </Card>
-          </Col>
         </Row>
         <br />
         <br />
