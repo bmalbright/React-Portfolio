0e15f4b5088c6830445a77dd00c8c31f492ca3dc
bmalbright <brianalbright@utexas.edu>
2022-06-17 14:09:25 -0500

    added a home page with an animation.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
-import AboutMe from './pages/Home';
+import AboutMe from './pages/About';
 import Projects from './pages/Projects';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
+import Home from './pages/Home';
 
 import "./css/App.css";
 
@@ -17,7 +17,8 @@ function App() {
         <div className='portfolio'>
           <Navbar />
           <Switch>
-            <Route exact path="/" component={AboutMe} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={AboutMe} />
             <Route exact path="/projects" component={Projects} />
             <Route exact path="/resume" component={Resume} />
             <Route exact path="/contact" component={Contact} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,80 +1,16 @@
-import React from "react";
-import { Col, Row, Card } from "react-bootstrap";
-import "../css/App.css";
-// import "../css/Home.css";
-
-import Biopic from "../images/biopic.jpg";
-import Fiona from "../images/fiona.jpg";
+import React from 'react'
+import "../css/Home.css";
+import  { Container } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <div className="home">
-      <Row>
-        <Col className="profile">
-          <Card className="biopic">
-            <img className="img-fluid" src={Biopic} alt="Biopic" />
-          </Card>
-        </Col>
-      </Row>
-
-      <div>
-        <Row>
-          <Col>
-            <Card className="bio">
-              <h1 className="intro"> Hi, I'm Brian</h1>
-              <p className="paragraph">
-                I am a full stack web developer with a management background
-                looking to build a more intuitive experience for the user. As a
-                recent graduate earning a certificate in Full Stack Web
-                Development from the University of Texas at Austin, I developed
-                skills using HTML, CSS, Javascript, React, MySQL, MongoDB, Node,
-                and Express.
-              </p>
-              <p className="paragraph">
-                My previous background in management gives me a unique
-                perspective on the user experience. Not only do I consider how
-                the project is being realized in the present, but also what
-                future development can be added to make the experience better.
-              </p>
-
-              {/* <p className="paragraph">My recent projects have included:</p>
-              <ul className="paragraph">
-                <li>
-                  A social media app that gives the user a task or challenge,
-                  which they complete by taking and uploading a pic
-                </li>
-                <li>
-                  A workout app that allows users to create and schedule their
-                  workouts
-                </li>
-              </ul> */}
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          {/* <Col> */}
-
-          {/* </Col> */}
-          {/* <Col> */}
-            <Card className="fiona">
-              <img className="img-fluid" src={Fiona} alt="Fiona" />
-            </Card>
-          {/* </Col> */}
-          <Card className="bio">
-              {/* <p className="paragraph">
-                I am excited to use my knowledge and experience to work on a
-                team building better experiences on the web - whether the user
-                accesses them on mobile devices or at their desk.
-              </p> */}
-              <p className="paragraph">
-                When not at my computer, I can be found at the park walking my
-                dog, Fiona.
-              </p>
-            </Card>
-        </Row>
-        <br />
-        <br />
-      </div>
-    </div>
-  );
+<Container>
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
+<div class="content">
+  <h1>Welcome to my portfolio</h1>
+</div>
+</Container>
+  )
 }
